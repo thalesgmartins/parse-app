501234f9b8a5f0de84c6ed7d1f2abca07e29fd73
thalesgmartins <thalesdiniz30@gmail.com>
2026-04-26 18:35:22 -0300

    add comentarios

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,13 @@
-"""Parse Main."""
+"""Parse Api+Web Main."""
 
 from fastapi import FastAPI
 
 from app.api import auth, cnis, web
 
+# Cria o objeto app que o FastAPI usa.
 app = FastAPI(title="Parse API")
 
+# Adiciona os routers do app
 app.include_router(cnis.router)
 app.include_router(auth.router)
 app.include_router(web.router)
