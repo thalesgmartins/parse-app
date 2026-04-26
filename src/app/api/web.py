e650732c60c7c4339ddecfb174d4d58eb8e55d9c
thalesgmartins <thalesdiniz30@gmail.com>
2026-04-26 18:36:22 -0300

    melhorias na página

--- src/app/api/web.py
+++ src/app/api/web.py
@@ -1,11 +1,30 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter(tags=["Frontend"])
+from app.api.auth import obter_usuario_logado
+from app.database.repository import listar_clientes
 
+router = APIRouter(tags=["Frontend"])
 templates = Jinja2Templates(directory="src/app/web/templates")
 
 
 @router.get("/dashboard")
 async def renderizar_dashboard(request: Request):
-    return templates.TemplateResponse(request=request, name="dashboard.html")
+    try:
+        usuario = await obter_usuario_logado(request.cookies.get("access_token"))
+
+        clientes_do_advogado = listar_clientes(usuario.id)
+
+        return templates.TemplateResponse(
+            request=request,
+            name="dashboard.html",
+            context={"clientes": clientes_do_advogado, "usuario_email": usuario.email},
+        )
+    except HTTPException:
+        return RedirectResponse(url="/login", status_code=303)
+
+
+@router.get("/login")
+async def renderizar_login(request: Request):
+    return templates.TemplateResponse(request=request, name="login.html")
